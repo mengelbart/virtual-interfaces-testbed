478d6ca919fa0657fb72f894aa766e814a3690fc
Mathis Engelbart <mathis.engelbart@gmail.com>
2024-05-29 11:06:38 +0200

    Add test setup from https://github.com/webrtc/samples

---
+++ test/interop/connection.test.js
@@ -0,0 +1,57 @@
+/*
+ *  Copyright (c) 2022 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree.
+ */
+const { buildDriver } = require('../webdriver');
+const { PeerConnection, MediaDevices } = require('../webrtcclient');
+const steps = require('../steps');
+
+const browserA = process.env.BROWSER_A || 'chrome';
+const browserB = process.env.BROWSER_B || 'chrome';
+
+describe(`basic interop test ${browserA} => ${browserB}`, function () {
+    let drivers;
+    let clients;
+    beforeAll(async () => {
+        const options = {
+            version: process.env.BVER || 'stable',
+            browserLogging: true,
+        }
+        drivers = [
+            await buildDriver(browserA, options, remoteUrl = 'http://10.1.0.10:8080'),
+            await buildDriver(browserB, options, remoteUrl = 'http://10.3.0.20:9090'),
+        ];
+        clients = drivers.map(driver => {
+            return {
+                connection: new PeerConnection(driver),
+                mediaDevices: new MediaDevices(driver),
+            };
+        });
+    });
+    afterAll(async () => {
+        await drivers.map(driver => driver.close());
+    });
+
+    it('establishes a connection', async () => {
+        await Promise.all(drivers); // timeouts in before(Each)?
+        await steps.step(drivers, (d) => d.get('http://localhost:8000/'), 'Empty page loaded');
+        await steps.step(clients, (client) => client.connection.create(), 'Created RTCPeerConnection');
+        await steps.step(clients, async (client) => {
+            const stream = await client.mediaDevices.getUserMedia({ audio: true, video: true });
+            return Promise.all(stream.getTracks().map(async track => {
+                return client.connection.addTrack(track, stream);
+            }));
+        }, 'Acquired and added audio/video stream');
+        const offerWithCandidates = await clients[0].connection.setLocalDescription();
+        await clients[1].connection.setRemoteDescription(offerWithCandidates);
+        const answerWithCandidates = await clients[1].connection.setLocalDescription();
+        await clients[0].connection.setRemoteDescription(answerWithCandidates);
+
+        await steps.step(drivers, (d) => steps.waitNVideosExist(d, 1), 'Video elements exist');
+        await steps.step(drivers, steps.waitAllVideosHaveEnoughData, 'Video elements have enough data');
+        await steps.step(clients, (client) => client.connection.sendDataChannel(), 'Created data channel');
+    }, 30000);
+}, 90000);
---
+++ test/webdriver.js
@@ -0,0 +1,132 @@
+/*
+ *  Copyright (c) 2022 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree.
+ */
+const os = require('os');
+const path = require('path');
+
+const webdriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const firefox = require('selenium-webdriver/firefox');
+const safari = require('selenium-webdriver/safari');
+
+async function download(browser, version, cacheDir, platform) {
+  const buildId = await puppeteerBrowsers
+    .resolveBuildId(browser, platform, version);
+  await puppeteerBrowsers.install({
+    browser,
+    buildId,
+    cacheDir,
+    platform
+  });
+  return buildId;
+}
+const cacheDir = path.join(process.cwd(), 'browsers');
+
+if (os.platform() === 'win32') {
+  process.env.PATH += ';' + process.cwd() + '\\node_modules\\chromedriver\\lib\\chromedriver\\';
+  process.env.PATH += ';' + process.cwd() + '\\node_modules\\geckodriver';
+} else {
+  process.env.PATH += ':node_modules/.bin';
+}
+
+function mapVersion(browser, version) {
+  const versionMap = {
+    chrome: {
+      unstable: 'dev',
+    },
+    firefox: {
+      unstable: 'nightly',
+    }
+  };
+  return (versionMap[browser] || {})[version] || version;
+}
+
+async function buildDriver(browser = process.env.BROWSER || 'chrome', options = { version: process.env.BVER }, remoteUrl = '') {
+  const version = mapVersion(options.version);
+
+  // Chrome options.
+  const chromeOptions = new chrome.Options()
+    .addArguments('allow-insecure-localhost')
+    .addArguments('use-fake-device-for-media-stream')
+    .addArguments('allow-file-access-from-files');
+  if (options.chromeFlags) {
+    options.chromeFlags.forEach((flag) => chromeOptions.addArguments(flag));
+  }
+  if (options.chromepath) {
+    chromeOptions.setChromeBinaryPath(options.chromepath);
+  }
+
+  if (!options.devices || options.headless) {
+    // GUM doesn't work in headless mode so we need this. See
+    // https://bugs.chromium.org/p/chromium/issues/detail?id=776649
+    chromeOptions.addArguments('use-fake-ui-for-media-stream');
+  } else {
+    // see https://bugs.chromium.org/p/chromium/issues/detail?id=459532#c22
+    const domain = 'https://' + (options.devices.domain || 'localhost') + ':' + (options.devices.port || 443) + ',*';
+    const exceptions = {
+      media_stream_mic: {},
+      media_stream_camera: {},
+    };
+
+    exceptions.media_stream_mic[domain] = {
+      last_used: Date.now(),
+      setting: options.devices.audio ? 1 : 2 // 0: ask, 1: allow, 2: denied
+    };
+    exceptions.media_stream_camera[domain] = {
+      last_used: Date.now(),
+      setting: options.devices.video ? 1 : 2
+    };
+
+    chromeOptions.setUserPreferences({
+      profile: {
+        content_settings: {
+          exceptions: exceptions
+        }
+      }
+    });
+  }
+
+  // Safari options.
+  const safariOptions = new safari.Options();
+  safariOptions.setTechnologyPreview(version === 'unstable');
+
+  // Firefox options.
+  const firefoxOptions = new firefox.Options();
+  let firefoxPath = firefox.Channel.RELEASE;
+  if (options.firefoxpath) {
+    firefoxPath = options.firefoxpath;
+  }
+  if (options.headless) {
+    firefoxOptions.addArguments('-headless');
+  }
+  firefoxOptions.setBinary(firefoxPath);
+  firefoxOptions.setPreference('media.navigator.streams.fake', true);
+  firefoxOptions.setPreference('media.navigator.permission.disabled', true);
+
+  let driver = new webdriver.Builder()
+    .setChromeOptions(chromeOptions)
+    .setSafariOptions(safariOptions)
+    .setFirefoxOptions(firefoxOptions)
+    .forBrowser(browser)
+    .setChromeService(
+      new chrome.ServiceBuilder().addArguments('--disable-build-check')
+    );
+
+  if (remoteUrl) {
+    driver = driver.usingServer(remoteUrl);
+  }
+
+  if (browser === 'firefox') {
+    driver.getCapabilities().set('marionette', true);
+    driver.getCapabilities().set('acceptInsecureCerts', true);
+  }
+  return driver.build();
+}
+
+module.exports = {
+  buildDriver,
+};
