6c13f74375d020e1ac418203e5015065b8009f59
Mathis Engelbart <mathis.engelbart@gmail.com>
2024-07-04 09:58:57 +0200

    Enable tc bandwidth/delay settings

--- webrtc/webrtctest.py
+++ webrtc/webrtctest.py
@@ -1,14 +1,16 @@
+import time
 from pathlib import Path
 from pyroute2 import NSPopen
 from subprocess import Popen
+from network.network import setup_tc, clear_tc
 
 WEBDRIVER_PATH = './webrtc/driver/chromedriver-linux64/chromedriver'
 
 def webrtctest():
-    output_dir = 'webrtctest'
-    Path(output_dir).mkdir(parents=True, exist_ok=True)
+    setup_tc()
     webserver_a = NSPopen('ns1', ['python', '-m', 'http.server'])
     webserver_b = NSPopen('ns4', ['python', '-m', 'http.server'])
+    time.sleep(1)
     webdriver_a = NSPopen('ns1', [WEBDRIVER_PATH, '--port=8080', '--allowed-ips', '--verbose', '--log-path=./webrtc/webdriver_a.log'])
     webdriver_b = NSPopen('ns4', [WEBDRIVER_PATH, '--port=8080', '--allowed-ips', '--verbose', '--log-path=./webrtc/webdriver_b.log'])
 
@@ -19,3 +21,4 @@ def webrtctest():
     webdriver_b.terminate()
     webserver_a.terminate()
     webserver_b.terminate()
+    clear_tc()
